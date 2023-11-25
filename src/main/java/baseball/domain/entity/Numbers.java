b7e6b36ac77abb58e69be592f2449305af302a14
inpink <inpink@kakao.com>
2023-11-25 19:08:15 +0900

    feat: 입력 숫자 검증

--- src/main/java/baseball/domain/entity/Numbers.java
+++ src/main/java/baseball/domain/entity/Numbers.java
@@ -1,6 +1,13 @@
 package baseball.domain.entity;
 
+import static baseball.constants.IntegerConstants.NUMBERS_LENGTH;
+import static baseball.constants.IntegerConstants.NUMBERS_MAX_RANGE;
+import static baseball.constants.IntegerConstants.NUMBERS_MIN_RAGNE;
+
+import baseball.util.ExceptionUtil;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Numbers {
     private final List<Integer> numbers;
@@ -15,7 +22,38 @@ public class Numbers {
     }
 
     private void validate(List<Integer> numbers) {
-
+        validateLength(numbers);
+        validateDuplicate(numbers);
+        validateNumberInRange(numbers);
     }
+
+    private void validateNumberInRange(List<Integer> numbers) {
+        boolean isInRange = numbers.stream()
+                .allMatch(number
+                        -> (number >= NUMBERS_MIN_RAGNE.getValue()
+                        && number <= NUMBERS_MAX_RANGE.getValue()));
+
+        if (!isInRange) {
+            ExceptionUtil.throwInvalidValueException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> removeDuplicate = numbers
+                .stream()
+                .collect(Collectors.toSet());
+
+        if (removeDuplicate.size() != numbers.size()) {
+            ExceptionUtil.throwInvalidValueException();
+
+        }
+    }
+
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() != NUMBERS_LENGTH.getValue()) {
+            ExceptionUtil.throwInvalidValueException();
+        }
+    }
+
 
 }
